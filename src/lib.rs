dddafa4af793dda908077ccbd25a911761a016ee
StudentWeis <studentweis@gmail.com>
2024-09-25 20:38:48 +0800

    0.1.6：菜单功能

---
+++ src/lib.rs
@@ -0,0 +1,138 @@
+use device_query::{ DeviceQuery, DeviceState, Keycode };
+use std::sync::mpsc::{ self, Receiver };
+use std::thread;
+use std::time::Instant;
+use eframe::egui;
+
+pub struct MyApp {
+    time: u128,
+    status_receiver: Receiver<(String, u128)>,
+    tick_flag: bool,
+    menu_config: MenuConfig,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        let (status_sender, status_receiver) = mpsc::channel();
+
+        // Spawn a new thread to handle key input and status updates
+        thread::spawn(move || {
+            let device_state = DeviceState::new();
+            let mut down_count = 0;
+            let mut status = "init".to_string();
+            let mut start_time = Instant::now();
+
+            loop {
+                let keys: Vec<Keycode> = device_state.get_keys();
+                if keys.is_empty() {
+                    down_count = 0;
+                } else {
+                    down_count += 1;
+                }
+
+                if down_count == 1 && keys.contains(&Keycode::LControl) {
+                    match status.as_str() {
+                        "init" => {
+                            status = "started".to_string();
+                            start_time = Instant::now();
+                            status_sender.send((status.clone(), 0)).unwrap();
+                        }
+                        "started" => {
+                            let elapsed_time = start_time.elapsed().as_millis();
+                            status = "init".to_string();
+                            status_sender.send((status.clone(), elapsed_time)).unwrap();
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        });
+        Self {
+            time: 0,
+            status_receiver,
+            tick_flag: false,
+            menu_config: MenuConfig::default(),
+        }
+    }
+}
+
+impl MyApp {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        load_fonts(&cc.egui_ctx);
+        Self::default()
+    }
+}
+
+fn load_fonts(ctx: &egui::Context) {
+    let mut fonts = egui::FontDefinitions::default();
+    fonts.font_data.insert(
+        "my_font".to_owned(),
+        egui::FontData::from_static(include_bytes!("C:\\Windows\\Fonts\\simsun.ttc"))
+    );
+    fonts.families
+        .get_mut(&egui::FontFamily::Proportional)
+        .unwrap()
+        .insert(0, "my_font".to_owned());
+    fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap().push("my_font".to_owned());
+    ctx.set_fonts(fonts);
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // 置顶功能
+        if self.menu_config.top {
+            ctx.send_viewport_cmd(
+                egui::ViewportCommand::WindowLevel(egui::WindowLevel::AlwaysOnTop)
+            );
+        } else {
+            ctx.send_viewport_cmd(egui::ViewportCommand::WindowLevel(egui::WindowLevel::Normal));
+        }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.heading("Rustick 计时器");
+                    ui.menu_button("菜单", |ui| {
+                        ui.checkbox(&mut self.menu_config.dark_mode, "黑暗模式");
+                        ui.checkbox(&mut self.menu_config.top, "置顶");
+                    });
+                    ui.menu_button("提示", |ui| {
+                        ui.label("按下左边的 Ctrl 开始计时\n再次按下结束计时");
+                    });
+                });
+            });
+            ui.separator();
+            ui.vertical_centered(|ui| {
+                ui.add_space(10.0);
+                ui.label(format!("{} 毫秒", self.time));
+                if self.tick_flag {
+                    ui.label("正在计时...");
+                } else {
+                    ui.label("未开始计时👌");
+                }
+            });
+        });
+
+        // 主题设置
+        if self.menu_config.dark_mode {
+            ctx.set_visuals(egui::Visuals::dark());
+        } else {
+            ctx.set_visuals(egui::Visuals::light());
+        }
+
+        // 计时消息
+        if let Ok((status, time)) = self.status_receiver.try_recv() {
+            if status == "init" {
+                self.time = time;
+                self.tick_flag = false;
+            } else {
+                self.tick_flag = true;
+            }
+        }
+    }
+}
+
+#[derive(Default)]
+struct MenuConfig {
+    dark_mode: bool,
+    top: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,11 @@
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use eframe::egui;
-use device_query::{ DeviceQuery, DeviceState, Keycode };
-use std::sync::mpsc::{ self, Receiver };
-use std::thread;
-use std::time::Instant;
+use rustick::MyApp;
 
 fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 140.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([250.0, 120.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -17,108 +14,4 @@ fn main() -> eframe::Result<()> {
         options,
         Box::new(|cc| Ok(Box::new(MyApp::new(cc))))
     )
-}
-
-struct MyApp {
-    time: u128,
-    status_receiver: Receiver<(String, u128)>,
-    flag: bool,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        let (status_sender, status_receiver) = mpsc::channel();
-
-        // Spawn a new thread to handle key input and status updates
-        thread::spawn(move || {
-            let device_state = DeviceState::new();
-            let mut down_count = 0;
-            let mut status = "init".to_string();
-            let mut start_time = Instant::now();
-
-            loop {
-                let keys: Vec<Keycode> = device_state.get_keys();
-                if keys.is_empty() {
-                    down_count = 0;
-                } else {
-                    down_count += 1;
-                }
-
-                if down_count == 1 && keys.contains(&Keycode::LControl) {
-                    match status.as_str() {
-                        "init" => {
-                            status = "started".to_string();
-                            start_time = Instant::now();
-                            status_sender.send((status.clone(), 0)).unwrap();
-                        }
-                        "started" => {
-                            let elapsed_time = start_time.elapsed().as_millis();
-                            status = "init".to_string();
-                            status_sender.send((status.clone(), elapsed_time)).unwrap();
-                        }
-                        _ => {}
-                    }
-                }
-            }
-        });
-
-        Self {
-            time: 0,
-            status_receiver,
-            flag: false,
-        }
-    }
-}
-
-impl MyApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        load_fonts(&cc.egui_ctx);
-        Self::default()
-    }
-}
-
-fn load_fonts(ctx: &egui::Context) {
-    let mut fonts = egui::FontDefinitions::default();
-    fonts.font_data.insert(
-        "my_font".to_owned(),
-        egui::FontData::from_static(include_bytes!("C:\\Windows\\Fonts\\simsun.ttc"))
-    );
-    fonts.families
-        .get_mut(&egui::FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "my_font".to_owned());
-    fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap().push("my_font".to_owned());
-    ctx.set_fonts(fonts);
-}
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Rustick 计时器\n按下左边的 Ctrl 开始计时\n再次按下结束计时");
-            ui.separator();
-            ui.label(format!("{} 毫秒", self.time));
-            if self.flag {
-                ui.label("正在计时");
-            } else {
-                ui.label("未开始计时");
-            }
-            ui.horizontal(|ui| {
-                if ui.button("暗").clicked() {
-                    ctx.set_visuals(egui::Visuals::dark());
-                }
-                if ui.button("亮").clicked() {
-                    ctx.set_visuals(egui::Visuals::light());
-                }
-            });
-        });
-
-        if let Ok((status, time)) = self.status_receiver.try_recv() {
-            if status == "init" {
-                self.time = time;
-                self.flag = false;
-            } else {
-                self.flag = true;
-            }
-        }
-    }
 }
